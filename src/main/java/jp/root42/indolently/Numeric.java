56b515f5328f9b838bca3445f868b05fc2b1e001
takahashikzn <takahashikzn@users.noreply.github.com>
2018-05-24 16:37:26 +0900

    RF

--- src/main/java/jp/root42/indolently/Numeric.java
+++ src/main/java/jp/root42/indolently/Numeric.java
@@ -15,6 +15,10 @@ package jp.root42.indolently;
 
 import java.math.BigDecimal;
 import java.util.function.Function;
+import java.util.function.Predicate;
+import java.util.function.ToDoubleFunction;
+import java.util.function.ToIntFunction;
+import java.util.function.ToLongFunction;
 
 
 /**
@@ -25,21 +29,21 @@ public final class Numeric {
     /** non private for subtyping. */
     protected Numeric() {}
 
-    public static int intOf(final String s) { return Integer.valueOf(s); }
+    public static int asInt(final String s) { return Integer.valueOf(s); }
 
-    public static long longOf(final String s) { return Long.valueOf(s); }
+    public static long asLong(final String s) { return Long.valueOf(s); }
 
-    public static short shortOf(final String s) { return Short.valueOf(s); }
+    public static short asShort(final String s) { return Short.valueOf(s); }
 
-    public static byte byteOf(final String s) { return Byte.valueOf(s); }
+    public static byte asByte(final String s) { return Byte.valueOf(s); }
 
-    public static float floatOf(final String s) { return Float.valueOf(s); }
+    public static float asFloat(final String s) { return Float.valueOf(s); }
 
-    public static double doubleOf(final String s) { return Double.valueOf(s); }
+    public static double asDouble(final String s) { return Double.valueOf(s); }
 
-    public static boolean boolOf(final String s) { return Boolean.valueOf(s); }
+    public static boolean asBool(final String s) { return Boolean.valueOf(s); }
 
-    public static char charOf(final String s) { return s.charAt(0); }
+    // public static char asChar(final String s) { return s.charAt(0); }
 
     public static BigDecimal decimal(final String s) { return new BigDecimal(s); }
 
@@ -47,21 +51,29 @@ public final class Numeric {
 
     public static BigDecimal decimal(final double d) { return BigDecimal.valueOf(d); }
 
-    public static Function<String, Integer> toInt() { return Integer::valueOf; }
+    public static Function<String, Integer> asInt() { return Numeric::asInt; }
 
-    public static Function<String, Long> toLong() { return Long::valueOf; }
+    public static ToIntFunction<String> toInt() { return Numeric::asInt; }
 
-    public static Function<String, Short> toShort() { return Short::valueOf; }
+    public static Function<String, Long> asLong() { return Numeric::asLong; }
 
-    public static Function<String, Byte> toByte() { return Byte::valueOf; }
+    public static ToLongFunction<String> toLong() { return Numeric::asLong; }
 
-    public static Function<String, Float> toFloat() { return Float::valueOf; }
+    public static Function<String, Short> asShort() { return Numeric::asShort; }
 
-    public static Function<String, Double> toDouble() { return Double::valueOf; }
+    public static Function<String, Byte> asByte() { return Numeric::asByte; }
 
-    public static Function<String, Boolean> toBool() { return Boolean::valueOf; }
+    public static Function<String, Float> asFloat() { return Numeric::asFloat; }
 
-    public static Function<String, Character> toChar() { return x -> charOf(x); }
+    public static Function<String, Double> asDouble() { return Numeric::asDouble; }
 
-    public static Function<String, BigDecimal> toDecimal() { return x -> decimal(x); }
+    public static ToDoubleFunction<String> toDouble() { return Numeric::asDouble; }
+
+    public static Function<String, Boolean> asBool() { return Numeric::asBool; }
+
+    public static Predicate<String> toBool() { return Numeric::asBool; }
+
+    // public static Function<String, Character> toChar() { return x -> asChar(x); }
+
+    public static Function<String, BigDecimal> decimal() { return x -> decimal(x); }
 }
